266aa860b8ce8c2601d63e03019ab9874f54e0d1
Mohamed Turki <t.momeedo@gmail.com>
2024-08-08 18:12:19 +0100

    Display Tasks

---
+++ src/http/task-api.js
@@ -0,0 +1,13 @@
+import api from "./api.js"
+
+const resource = '/tasks'
+
+export const allTasks = () => api.get(resource)
+
+export const createTask = task => api.post(resource, task)
+
+export const updateTask = (id, task) => api.put(`${resource}/${id}`, task)
+
+export const removeTask = task => api.delete(`${resource}/${id}`)
+
+export const completeTask = (id, task) => api.patch(`${resource}/${id}/complete`, task)
