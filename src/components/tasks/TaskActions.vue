504a692e670c05ec1302807324a9bf42cc108099
Mohamed Turki <t.momeedo@gmail.com>
2024-08-09 13:29:48 +0100

    Edit Template

---
+++ src/components/tasks/TaskActions.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="task-actions">
+        <button class="btn btn-sm btn-circle btn-outline-secondary me-1" @click="$event => emit('edit')">
+            <IconPencil />
+        </button>
+        <button class="btn btn-sm btn-circle btn-outline-danger" @click="$event => emit('delete')">
+            <IconBin />
+        </button>
+    </div>
+</template>
+
+<script setup>
+import IconBin from '../icons/IconBin.vue'
+import IconPencil from '../icons/IconPencil.vue'
+
+const emit = defineEmits(['edit', 'remove'])
+</script>
