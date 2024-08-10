1eee35b40fe614ee4a5db796e152a841004af1f9
Mohamed Turki <t.momeedo@gmail.com>
2024-08-11 00:39:36 +0100

    Setting .env Variables

--- src/http/task-api.js
+++ src/http/task-api.js
@@ -1,13 +1,16 @@
-import api from "./api.js"
+import api from "./api.js";
 
-const resource = '/tasks'
+const apiPath = import.meta.env.VITE_API_PATH;
+const resource = apiPath + "/tasks";
 
-export const allTasks = () => api.get(resource)
 
-export const createTask = (task) => api.post(resource, task)
+export const allTasks = () => api.get(resource);
 
-export const updateTask = (id, task) => api.put(`${resource}/${id}`, task)
+export const createTask = (task) => api.post(resource, task);
 
-export const deleteTask = id => api.delete(`${resource}/${id}`)
+export const updateTask = (id, task) => api.put(`${resource}/${id}`, task);
 
-export const completeTask = (id, task) => api.patch(`${resource}/${id}/complete`, task)
+export const deleteTask = (id) => api.delete(`${resource}/${id}`);
+
+export const completeTask = (id, task) =>
+  api.patch(`${resource}/${id}/complete`, task);
--- src/router/router.js
+++ src/router/router.js
@@ -16,9 +16,9 @@ const routes = [
     path: "/tasks",
     component: TasksPage,
     name: "tasks",
-    meta: {
-      auth: true,
-    },
+    // meta: {
+    //   auth: true,
+    // },
   },
   {
     path: "/login",
